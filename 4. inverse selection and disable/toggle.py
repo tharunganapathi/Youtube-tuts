e9c8ac721879ac88ae35413bdcd54c1629704b49
tharun ganapathi <65713157+tharunganapathi@users.noreply.github.com>
2023-01-01 02:01:35 +0530

    Inverse selection and disable

---
+++ 4. inverse selection and disable/toggle.py
@@ -0,0 +1,35 @@
+﻿
+import nuke
+import nukescripts
+
+
+
+def toggle_disable():
+
+    if len(nuke.selectedNodes()) == 0:
+        nuke.message("Please select the nodes")
+
+    else:
+        for i in nuke.selectedNodes():
+            try:
+                #print i.name()
+                    
+                if i['disable'].value() == True:
+                    i['disable'].setValue(False)
+                else:
+                    i['disable'].setValue(True)
+            except:
+                pass
+
+
+def toggle_selection():
+
+    if len(nuke.selectedNodes()) == 0:
+        nuke.selectAll()
+
+    else:
+    	nuke.invertSelection()
+
+            
+nuke.menu('Nuke').addMenu('TG').addCommand('toggle/toggle_disable', toggle_disable, 'Shift+D')
+nuke.menu('Nuke').addMenu('TG').addCommand('toggle/toggle_selection', toggle_selection, 'Shift+A')
