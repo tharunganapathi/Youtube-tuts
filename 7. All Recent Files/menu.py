c14f102f3e00eb8f8b76a14c9639f749bc857220
tharun ganapathi <65713157+tharunganapathi@users.noreply.github.com>
2023-01-28 02:48:29 +0530

    Open all recent files

---
+++ 7. All Recent Files/menu.py
@@ -0,0 +1,1 @@
+﻿import recent_files
---
+++ 7. All Recent Files/recent_files.py
@@ -0,0 +1,25 @@
+import nuke
+import os
+import sys
+
+def recent_files():
+	user = os.path.expanduser('~')
+	dot_nuke = user + '\.nuke'
+
+	recent_files = os.path.abspath(os.path.join(dot_nuke,'recent_files') )
+
+
+	text = open(recent_files,"r")
+	recentFile_list = (text.read())
+	print (recentFile_list)
+
+
+	p = nuke.Panel('RecentFiles v1.0')
+	p.addEnumerationPulldown('Open Recent Files', recentFile_list)
+
+	if p.show():
+	    nuke.scriptOpen(p.value('Open Recent Files'))
+
+
+nuke.menu('Nuke').addMenu('TG').addCommand('Recent_files v1.0', recent_files)
+
